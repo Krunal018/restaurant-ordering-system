9fe5ee11f88d377a416f05d7f928f0be68ea2ebc
Krunal018 <krunalmangukiya18@gmail.com>
2025-08-06 15:19:22 +0530

    change in path

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,16 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
-const path = require('path');
+const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
 // module.exports = defineConfig({
 //   transpileDependencies: true
 // });
 
 module.exports = {
-  outputDir: path.resolve(__dirname, '../backend/restaurant_management'),
+  outputDir: path.resolve(__dirname, "../backend/restaurant_management"),
   devServer: {
     proxy: {
-      '/': {
-        target: 'http://localhost:8001'
-      }
-    }
-  }
-}
+      "/": {
+        target: "https://restaurant-ordering-system-c28u.onrender.com",
+      },
+    },
+  },
+};
