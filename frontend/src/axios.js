ad3e8cf990287b7336535ba91d8ce34cebf53f76
Krunal018 <krunalmangukiya18@gmail.com>
2025-08-06 15:34:08 +0530

    initial commit

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 window.axios = axios;
 axios.defaults.withCredentials = false;
-// axios.defaults.baseURL = "http://localhost:8000/api"
 let backendUrl = "https://restaurant-ordering-system-c28u.onrender.com/api";
-axios.defaults.baseURL = backendUrl;
+axios.defaults.baseURL =
+  "https://restaurant-ordering-system-c28u.onrender.com/api";
